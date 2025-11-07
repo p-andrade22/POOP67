20850c333594cac25d5abe6c4839652c65addac4
p-andrade22 <pabloandradeandres28@gmail.com>
2025-11-06 22:16:12 -0500

    Add files via upload

---
+++ clases.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+class Inteligencia_Artificial:
+    def __init__(self, nombre, tipo, lenguaje, capacidad):
+        self.nombre=nombre
+        self.tipo=tipo
+        self.lenguaje=lenguaje
+        self.capacidad=capacidad
+        
+    def atrinutos (self):
+        print(self.nombre)
+        print(".Tipo: ", self.tipo)
+        print(".Lenguaje: ", self.lenguaje)
+        print(".Capacidad: ", self.capacidad)
+        
+
+Aobjet1= Inteligencia_Artificial("Gemini", 1 , "Generativo", "32K")
+Aobjet2=Inteligencia_Artificial("ChatGpt", 2, "Generativo", "32K")
+Aobjet3=Inteligencia_Artificial("BlackBOx", 3, "Generativo", "32K")
+Aobjet4=Inteligencia_Artificial("Cloude", 4, "Generativo", "32K")
+Aobjet1.atrinutos()
+Aobjet2.atrinutos()
+Aobjet3.atrinutos()
+Aobjet4.atrinutos()
+
+class Dispositivo_ioT:
+    def __init__(self,sensor,microcontrolador,conectividad,fuente_energia):
+        self.sensor=sensor
+        self.microcontrolador=microcontrolador
+        self.conectividad=conectividad
+        self.fuente_energia=fuente_energia
+    def controlar():
+        pass
+    def Obtencion_de_datos():
+        pass
+    def ubicacion():
+        pass
+    def estado ():
+        pass
+Bobjet1=Dispositivo_ioT("Termostato Inteligente", "ESP32", "WiFi", "Electrica")
+print(Bobjet1.sensor, Bobjet1.microcontrolador,Bobjet1.conectividad,Bobjet1.fuente_energia)
+
+class Robot:
+    def __init__(self,unidad,fuente,estructura,sensores):
+        self.unidad=unidad
+        self.fuente=fuente
+        self.estructura=estructura
+        self.sensores=sensores
+    def AccionMovimiento():
+        pass
+    def Adaptabilidad():
+        pass
+    def Control():
+        pass
+    def Comunicacion():
+        pass
+Cobjet1=Robot("Arduino","Energia","Mecanica","Senseores Infrarojos")
+print(Cobjet1.unidad, Cobjet1.fuente, Cobjet1.estructura, Cobjet1.sensores)
+
+
+class RedSocial:
+    def __init__(self,nombre,cotenido,origen,usuarios, version):
+        self.nombre=nombre
+        self.cotenido=cotenido
+        self.origen=origen
+        self.usuarios=usuarios
+        self.version=version
+    def registrar():
+        pass
+    def subircontenido():
+        pass
+    def reaccionar():
+        pass
+    def seguir():
+        pass
+    def ActualizarVersion():
+        pass
+Dobject1= RedSocial("Instagram","Fotos Videos","EE.UU", "200Millones","386.0")
+print(Dobject1.nombre, Dobject1.cotenido, Dobject1.origen, Dobject1.usuarios, Dobject1.version)
+
+class PC :
+    def __init__ (self,procesador,ram,almacenamiento,tarjeta_grafica,sistema_operativo):
+        self.procesador=procesador
+        self.ram=ram
+        self.almacenamiento=almacenamiento
+        self.tarjeta_grafica=tarjeta_grafica
+        self.sistema_operativo=sistema_operativo
+    def EjecutarPrograma():
+        pass
+    def CargarDatos():
+        pass
+    def Guardaracrhivos():
+        pass
+    def ProcesarImagenes():
+        pass
+    def SistemaOperativo():
+        pass
+Eobject1=PC("Ip", 16, 1, 3080, "Windows")
+print(Eobject1.procesador, Eobject1.ram, Eobject1.almacenamiento, Eobject1.tarjeta_grafica, Eobject1.sistema_operativo)
+
+class Navegador:
+    def __init__ (self,motor_busqueda,historial,marcadores,pestañas):
+        self.motor_busqueda=motor_busqueda
+        self.historial=historial
+        self.marcadores=marcadores
+        self.pestañas=pestañas
+    def buscar():
+        pass
+    def mostrarhisorial():
+        pass
+    def guadarpagina():
+        pass
+    def abrirnuevapestaña():
+        pass
+Fobject1=Navegador("GOOGLE",1,2,3)
+print(Fobject1.motor_busqueda, Fobject1.historial, Fobject1.marcadores, Fobject1.pestañas)
+
+class Telefono:
+    def __init__(self,marca,modelo,sistema,almacenamientoo,bateria):
+        self.marca=marca
+        self.modelo=modelo
+        self.sistema=sistema
+        self.almacenamiento=almacenamientoo
+        self.bateria=bateria
+    def llamar():
+        pass
+    def enviar_mensaje():
+        pass
+    def enceder():
+        pass
+    def tomar_fotos():
+        pass
+    def cargar ():
+        pass
+TObject1= Telefono("iPhone",14,"IOS", 12, 100)
+print(TObject1.marca, TObject1.modelo,TObject1.sistema, TObject1.almacenamiento,TObject1.bateria)
+
+class Cine:
+    def __init__(self,titulo,director,genero,duracion,año):
+        self.titulo=titulo
+        self.director=director
+        self.genero=genero
+        self.duracion=duracion
+        self.año=año
+    def reproducion():
+        pass
+    def pausar():
+        pass
+    def calificar():
+        pass
+    def recomendar():
+        pass
+    def guardar():
+        pass
+Uobject1=Cine("Mago de Oz", "X", "Dramatico", "1 hora", "1890")
+print(Uobject1.titulo,Uobject1.director,Uobject1.genero,Uobject1.duracion,Uobject1.año)
+
+class Arbol:
+    def __init__(self,tipo,frutos,altura):
+        self.tipo=tipo
+        self.frutos=frutos
+        self.altura=altura
+    def brotar():
+        pass
+    def florecer():
+        pass
+    def Morir():
+        pass
+Manzano=Arbol("Manzano", "Manzanas", 1)
+print(Manzano.tipo, Manzano.frutos, Manzano.altura)
+
+
+
+
+
+
+
